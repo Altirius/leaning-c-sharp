2f2774419e748a114f0ceed2e7aa57c42f1b37e3
Altirius <afinamoon@gmail.com>
2022-12-11 23:46:45 +0300

    Zadanie-31

--- 5_занятие/31/Program.cs
+++ 5_занятие/31/Program.cs
@@ -3,22 +3,31 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6]
 //  сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
-Random rnd = new Random();
-int[] arrayNumbers = new int[12];
-for (int i = 0; i < 12; i++)
+void arraySumPM()
 {
-	arrayNumbers[i] = rnd.Next(-9, 9);
-}
-int sumMinus = 0;
-int sumPlus = 0;
-for (int i = 0; i < 12; i++)
+	Random rnd = new Random();
+	int[] arrayNumbers = new int[12];
+	for (int i = 0; i < 12; i++)
+	{
+		arrayNumbers[i] = rnd.Next(-9, 9);
+	}
+	int sumMinus = 0;
+	int sumPlus = 0;
+	for (int i = 0; i < 12; i++)
+	{
+		if (arrayNumbers[i] < 0)
+		{
+			sumMinus = sumMinus + arrayNumbers[i];
+		}
+		else
+		{
+			sumPlus = sumPlus + arrayNumbers[i];
+		}
+	}
+for (int i=0; i<12; i++)
 {
-	if (arrayNumbers[i] < 0)
-	{
-		sumMinus = sumMinus + arrayNumbers[i];
-	}
-	else
-	{
-		sumPlus = sumPlus + arrayNumbers[i];
-	}
+	Console.Write(" "+arrayNumbers[i]);
 }
+Console.WriteLine(" Сумма отрицательных чисел: "+sumMinus +" Сумма положительных чисел:"+sumPlus);
+}
+arraySumPM();
