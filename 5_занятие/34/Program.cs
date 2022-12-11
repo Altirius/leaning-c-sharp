99885f5ae854146e080e1bee25ab7f9e52f63f58
Altirius <afinamoon@gmail.com>
2022-12-12 01:09:08 +0300

    Zadanie-34

---
+++ 5_занятие/34/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void arrayShowCN()
+{
+	Random rnd = new Random();
+	int[] arrayNumbers = new int[10];
+	int a = 0;
+	for (int i = 0; i < arrayNumbers.Length; i++)
+	{
+		arrayNumbers[i] = rnd.Next(100, 999);
+		Console.Write(" " + arrayNumbers[i]);
+	}
+	for (int i=0; i<arrayNumbers.Length; i++)
+	{
+		if ((arrayNumbers[i]%2)==0)
+		{
+			a++;
+		}
+	}
+	Console.WriteLine();
+	Console.WriteLine("Количество черных числе в массиве: "+a);
+}
+arrayShowCN();
