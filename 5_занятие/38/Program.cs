dfb2a53f5f42d7b6a1bb6afd4c1545f0973cd494
Altirius <afinamoon@gmail.com>
2022-12-12 01:31:43 +0300

    Zadanie-38

---
+++ 5_занятие/38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void arrayRazn()
+{
+	Random rnd = new Random();
+	int[] arrayNumbers = new int[10];
+	int max=arrayNumbers[0];
+	for (int i = 0; i < arrayNumbers.Length; i++)
+	{
+		arrayNumbers[i] = rnd.Next(0, 100);
+		Console.Write(" " + arrayNumbers[i]);
+	}
+	for (int i=0; i<arrayNumbers.Length; i++)
+	{
+		if (arrayNumbers[i] > max)
+		{
+			max = arrayNumbers[i];
+		}
+	}
+	int min=max;
+	for (int i = 0; i < arrayNumbers.Length; i++)
+	{
+		if (arrayNumbers[i] < min)
+		{
+			min = arrayNumbers[i];
+		}
+	}
+	Console.WriteLine();
+	Console.WriteLine(" Разница между максимальным и минимальным значениями составляет вот столько: "+(max-min));
+}
+arrayRazn();
