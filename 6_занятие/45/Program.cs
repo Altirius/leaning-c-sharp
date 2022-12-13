1e6d58781759c1d3adff638ef9d8ea2b339f45e9
Altirius <afinamoon@gmail.com>
2022-12-13 22:12:35 +0300

    Zadanie-45

---
+++ 6_занятие/45/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования.
+
+
+void copyDefaultArray()
+{
+	int[] arrayOriginal = new int[7] { 3, 7, 9, 13, 23, 29, 97 };
+	int[] arrayCopy = new int[arrayOriginal.Length];
+	for (int i = 0; i < arrayOriginal.Length; i++)
+	{
+		arrayCopy[i] = arrayOriginal[i];
+		Console.Write(" "+arrayCopy[i]);
+	}
+}
+copyDefaultArray();
