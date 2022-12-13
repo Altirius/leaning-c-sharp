a4b5b7dcf1aaed780df5f28bab1ddddca28ec37a
Altirius <afinamoon@gmail.com>
2022-12-13 23:05:16 +0300

    Zadanie-41

---
+++ 6_занятие/41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void usersRandomNumbers()
+{
+	int result = 0;
+	string? input=Console.ReadLine();
+	while (input != "stop")
+	{
+		if (Convert.ToInt32(input) > 0)
+		{
+			result++;
+		}
+		input=Console.ReadLine();
+	}
+	Console.WriteLine("Воот столько положительных чисел Вы ввели:"+result);
+}
+usersRandomNumbers();
