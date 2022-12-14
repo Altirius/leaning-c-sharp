5bcba253da2b6dc808fcbfb2cca94f6940a084fe
Altirius <afinamoon@gmail.com>
2022-12-14 20:51:26 +0300

    Zadanie-42

---
+++ 6_занятие/42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void convert010()
+{
+	Console.WriteLine("Введите число вот сюда, пожалуйста:");
+	int number = Convert.ToInt32(Console.ReadLine());
+	string number010 = "";
+	while (number > 0)
+	{
+		if ((number % 2) == 1)
+		{
+			number010 = "1" + number010;
+		}
+		else
+		{
+			number010 = "0" + number010;
+		}
+		number = number / 2;
+	}
+	Console.WriteLine("Оказывается, Ваше число в двоичном виде вот такое: " + number010);
+}
+convert010();
