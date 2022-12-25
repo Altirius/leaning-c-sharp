fe2cf745fca5053d8519b49ec1c3ec66073f4638
Altirius <afinamoon@gmail.com>
2022-12-25 21:06:51 +0300

    Zadanie-63

---
+++ 9_занятие/63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void writeNaturalNumber(int N)
+{
+	if (N > 1)
+	{
+		writeNaturalNumber(N - 1);
+	}
+	Console.Write(N+", ");
+}
+
+
+writeNaturalNumber(5);
