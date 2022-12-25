45f6bb55be5f0b010ae875cb9d9a2246fe1a59c8
Altirius <afinamoon@gmail.com>
2022-12-25 21:58:48 +0300

    Zadanie-67

---
+++ 9_занятие/67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int sumNumberDigits(int M)
+{
+	if (M > 0)
+	{
+		return M % 10 + sumNumberDigits(M / 10);
+	}
+	return 0;
+}
+
+
+Console.Write(sumNumberDigits(453));
