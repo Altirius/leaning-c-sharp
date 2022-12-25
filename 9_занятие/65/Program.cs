9f60ac618b48712c9373aa3fc82862f3123f7d28
Altirius <afinamoon@gmail.com>
2022-12-25 21:43:40 +0300

    Zadanie-65

---
+++ 9_занятие/65/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5-> "1, 2, 3, 4, 5"
+// M = 4; N = 8-> "4, 5, 6, 7, 8"
+
+void writeNaturalNumber(int M, int N)
+{
+	Console.Write(M + ", ");
+	if (M < N)
+	{
+		writeNaturalNumber(M+1, N);
+	}
+}
+
+
+writeNaturalNumber(1, 5);
